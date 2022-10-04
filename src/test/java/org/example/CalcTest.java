279b15ecde96506ed728b0a4f276c289032d11db
MicroHenry <microhenry@hotmail.com>
2022-10-04 15:31:14 +0800

    Add the second test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -9,4 +9,9 @@ public class CalcTest {
         Calc c = new Calc();
         assertEquals(4, c.add(2,2));
     }
+    @Test
+    void testSubtract() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
+    }
 }
